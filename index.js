9eff2faf8e2c28ee4d19bf93b5dbd21b36c82ee7
doniphane <trulesdoniphane974@gmail.com>
2023-09-27 15:58:28 +0400

    code mise a jour V2

--- index.js
+++ index.js
@@ -10,7 +10,45 @@ let connection = mysql.createConnection({
   connection.connect((err)=>{
     if(err) throw err
     console.log("ok")
-
+    getAllTasks();
+    getTaskFromIdUser();
+ 
 
   });
+
+  function getAllTasks() {
+    const sqlselection = "SELECT * FROM tasks";
+    connection.query(sqlselection, (err, results) => {
+        if(err) {
+            console.error("Erreur de recherche dans la base de donée:", err);
+            return;
+        }
+
+        console.log("liste des task:");
+        console.log(results);
+
+      
+        // connection.end();
+    });
+}
+
+function  getTaskFromIdUser() {
+  const sqlselection = "SELECT * FROM USER";
+  connection.query(sqlselection, (err, results) => {
+      if(err) {
+          console.error("Erreur de recherche dans la base de donée:", err);
+          return;
+      }
+
+      console.log("liste des USER:");
+      console.log(results);
+
+    
+      connection.end();
+  });
+}
+
+
+
+
 
