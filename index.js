c264ecd5d6fee5833bf32b54df12b64f8d09cbfb
doniphane <trulesdoniphane974@gmail.com>
2023-09-27 11:14:19 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2');
+
+// Paramètres de connexion
+const dbConfig = {
+  host: 'localhost',
+  user: 'your-username',
+  password: 'your-password',
+  database: 'your-database'
+};
